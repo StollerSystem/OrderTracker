56d10ddbf3cab9c556297ac5fd73c02dda38cb9d
Ben Stoller <stollersystemsound@gmail.com>
2020-10-02 14:37:49 -0700

    start building controller to handle deleting orders

--- OrderTracker/Controllers/OrdersController.cs
+++ OrderTracker/Controllers/OrdersController.cs
@@ -23,5 +23,14 @@ namespace OrderTracker.Controllers
       model.Add("vendor", vendor);
       return View(model);
     }
+
+    [HttpPost("/vendors/{vendorId}/orders/{orderId}/delete")]
+    public ActionResult Delete(int vendorId, int orderId)
+    {
+      //Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      vendor.RemoveOrder(orderId);
+      return View();
+    }
   }
 }
