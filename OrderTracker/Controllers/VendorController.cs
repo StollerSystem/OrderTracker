51036e723badc6ee3bdd2596a6d3d426437b0603
Ben Stoller <stollersystemsound@gmail.com>
2020-10-02 10:16:11 -0700

    build controller and initial html for vendors page

---
+++ OrderTracker/Controllers/VendorController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using OrderTracker.Models;
+
+namespace OrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> vendors = Vendor.GetAll();
+      return View(vendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName,vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+  }
+}
