1dd5a300e5480121fc4b460bcb8e0d57699d9b91
Ben Stoller <stollersystemsound@gmail.com>
2020-10-02 08:49:01 -0700

    pass test for spec 1

--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -6,7 +6,7 @@ using System;
 namespace OrderTracker.Tests
 {
   [TestClass]
-  public class CategoryTest : IDisposable
+  public class VendorTest : IDisposable
   {
 
     public void Dispose()
@@ -19,6 +19,20 @@ namespace OrderTracker.Tests
     {
       Vendor newVendor = new Vendor();
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+
+      //Act
+      string result = newVendor.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
     }
 
 
