83240fef18482f740866127efbd334153eaa3b3b
Ben Stoller <stollersystemsound@gmail.com>
2020-10-02 08:48:02 -0700

    set up tests for vendor class

---
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace OrderTracker.Tests
+{
+  [TestClass]
+  public class CategoryTest : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor();
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+
+  }
+}
---
+++ OrderTracker/Models/Vendor.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+
+namespace OrderTracker.Models
+{
+  public class Vendor 
+  {
+    public static void ClearAll()
+    {
+      // _instances.Clear();
+    }
+
+    public Vendor()
+    {
+
+    }
+  }
+}
